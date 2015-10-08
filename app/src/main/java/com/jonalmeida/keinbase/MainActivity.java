b8bd2d84f8c510a3233ed6a8e4a17122d8190a27
Jonathan Almeida <git@jonalmeida.com>
2015-10-08 19:07:30 -0400

    Unused imports

--- app/src/main/java/com/jonalmeida/keinbase/MainActivity.java
+++ app/src/main/java/com/jonalmeida/keinbase/MainActivity.java
@@ -3,12 +3,8 @@ package com.jonalmeida.keinbase;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.internal.app.ToolbarActionBar;
-import android.support.v7.internal.widget.TintManager;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
