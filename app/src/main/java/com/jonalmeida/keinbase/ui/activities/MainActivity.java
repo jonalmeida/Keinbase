736d10953994aa96b4584a72a741942ad101756b
Jonathan Almeida <git@jonalmeida.com>
2015-10-22 00:04:07 -0400

    Added NavigationDrawer to MainActivity

--- app/src/main/java/com/jonalmeida/keinbase/ui/activities/MainActivity.java
+++ app/src/main/java/com/jonalmeida/keinbase/ui/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package com.jonalmeida.keinbase.ui.activities;
 
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -19,26 +22,33 @@ public class MainActivity extends AppCompatActivity {
     static final String REDIRECT_URI = "keinbase://coinbase-oauth";
     private static final String CLIENT_ID = "2e9bcf464cd4054c80a2a27cd49ef70934ff68aceef93d176f57ac4922463fb1";
 
-    ViewPager mViewPager;
-    PagerAdapter mPageAdapter;
-    TabLayout mTabLayout;
-    Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ViewPager mViewPager;
+    private PagerAdapter mPageAdapter;
+    private TabLayout mTabLayout;
+    private Toolbar mToolbar;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mTabLayout = (TabLayout) findViewById(R.id.tabs);
-        mToolbar   = (Toolbar)   findViewById(R.id.toolbar);
+        mDrawerLayout   = (DrawerLayout)    findViewById(R.id.drawer_layout);
+        mViewPager      = (ViewPager)       findViewById(R.id.pager);
+        mTabLayout      = (TabLayout)       findViewById(R.id.tabs);
+        mToolbar        = (Toolbar)         findViewById(R.id.toolbar);
+        mNavigationView = (NavigationView)  findViewById(R.id.nav_view);
         mPageAdapter = new MainViewPagerAdapter(getSupportFragmentManager());
 
-
+        if (mNavigationView != null) {
+            setupDrawerContent(mNavigationView);
+        }
         mViewPager.setAdapter(mPageAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -50,17 +60,24 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+            new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    menuItem.setChecked(true);
+                    mDrawerLayout.closeDrawers();
+                    return true;
+                }
+            });
     }
 //        try {
 //            OAuth.beginAuthorization(this, CLIENT_ID, "user", REDIRECT_URI, null);
